55babb8308aa5855f5c0ab069bd56d48d34612db
dmkisel <i@dkisel.ru>
2024-02-27 21:22:53 +0900

    Data type fix

--- homework_01/main.py
+++ homework_01/main.py
@@ -23,7 +23,7 @@ def is_prime(num):
        функция для проверки входящих чисел на простое число
     """
     if num > 1:
-        for i in range(2,(num/2+1)):
+        for i in range(2,int(num/2+1)):
             if num % i == 0:
                 return False
             else:
