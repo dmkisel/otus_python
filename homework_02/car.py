fe5fead3db79cfb897e56e7b1e482f6efc728879
Dmitry <i@dkisel.ru>
2024-03-14 12:33:28 +0900

    fix Engine

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,6 +2,7 @@
 создайте класс `Car`, наследник `Vehicle`
 """
 from homework_02.base import Vehicle
+from homework_02.engine import Engine
 
 class Car(Vehicle):
     def __init__(self, weight=0, fuel=0, fuel_consumption=0):
