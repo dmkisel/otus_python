2afa15d3acc64988a5b60c884f306701d0bce05b
Dmitry <i@dkisel.ru>
2024-05-24 22:45:27 +0900

    add css in /

--- homework_05/app.py
+++ homework_05/app.py
@@ -15,14 +15,19 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+program_url = ['https://cdn.otus.ru/media/public/be/cf/becf7d85261a4b9db10a56f8d187c849.png',
+               'https://cdn.otus.ru/media/public/87/cd/87cd08ade5884958a72a92b54fb06f73.png',
+               'https://cdn.otus.ru/media/public/b3/f4/b3f4563a92be4f6fbe7be943d19ac4b5.png']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', image_url = program_url)
 
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
